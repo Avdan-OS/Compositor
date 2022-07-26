e9d1786819df095ca114a8fe5acebc8a937ab23e
Froxcey <danichen204@gmail.com>
2022-07-27 02:36:30 +0800

    Add config_loader

---
+++ src/config_loader.rs
@@ -0,0 +1,19 @@
+use std::fs;
+use std::io::BufReader;
+use std::error::Error;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    test: String
+}
+
+pub fn read_config() -> Result<Config, Box<dyn Error>> {
+    fs::create_dir_all("/etc/AvdanOS").expect("Error while create AvdanOS config directory");
+    let file = fs::OpenOptions::new().read(true).write(true).create(true).open("/etc/AvdanOS/Compositor.json")?;
+    let reader = BufReader::new(file);
+    let u = serde_json::from_reader(reader)?;
+    Ok(u)
+}
+
