16c6d4936917f11a6c4eabcfb5ef7d45462f0abc
Sammy99jsp <sammy99jsp@gmail.com>
2022-08-02 00:29:58 +0100

    Updated build script: - Use of CONSTANTS. - Manually setting environment variable `OVERWRITE=false` will prevent config from being overwritten

--- build.rs
+++ build.rs
@@ -1,7 +1,31 @@
-use std::fs;
+use std::{fs, path::Path, env,};
+
+const CONFIG_FOLDER: &'static str = "/etc/AvdanOS";
+const CONFIG_FILE: &'static str = "Compositor.json";
 
 fn main() -> std::io::Result<()> {
-    fs::create_dir("/etc/AvdanOS")?;
-    fs::copy("Compositor.json", "/etc/AvdanOS/Compositor.json")?;
+    let config_path = Path::new(CONFIG_FOLDER)
+        .join(CONFIG_FILE);
+
+    fs::create_dir_all(CONFIG_FOLDER)?;
+    
+    overwrite_if_set(&config_path)
+}
+
+///
+/// Overwrites the config file unless
+/// OVERWRITE is manually set to `false`, or any non-`true` value.
+/// 
+fn overwrite_if_set(path: &Path) -> Result<(), std::io::Error> {
+    let overwrite = env::var("OVERWRITE")
+        .unwrap_or("true".into());
+    
+
+    if  Path::exists(path) &&
+        overwrite.to_lowercase().eq("true")
+    {
+        fs::remove_file(path)?;
+        fs::copy(CONFIG_FILE, path)?;
+    }
     Ok(())
 }
