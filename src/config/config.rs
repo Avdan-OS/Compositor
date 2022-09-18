451374529b86605c5926cc10141ca9faf3a64b8f
Akane <96370213+akane6704@users.noreply.github.com>
2022-09-18 21:18:30 +0200

    Guidelines & Types

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,61 +1,67 @@
-use std::{
-    error::Error, 
-    fs::{self, File},
-    io::BufReader,
-    collections::HashMap
+use crate::{
+    config::errors::UnexpectedToken,
+    CONST::{
+        CONFIG_FOLDER,
+        CONFIG_FILE,
+    },
 };
-
-use lazy_static::lazy_static;
-
 
 pub(crate) use json_comments::StripComments;
 
-use json_tree::{Index,};
+use lazy_static::lazy_static;
+
+use json_tree::{
+    JSONPath,
+    Index,
+    Indexer,
+    Location,
+    parser::Token,
+    Source,
+    Tokenizer,
+    Value,
+};
+
 use serde::Deserialize;
 
-use crate::{CONST::{
-    CONFIG_FOLDER,
-    CONFIG_FILE,
-}, config::errors::UnexpectedToken};
+use std::{
+    collections::HashMap,
+    error::Error, 
+    fs::{
+        File,
+        self,
+    },
+    io::BufReader,
+};
 
-use super::sections::{keybinds::Keybinds};
+use super::sections::keybinds::Keybinds;
 
 lazy_static! {
-    pub static ref PATH : String = CONFIG_FOLDER.join(*CONFIG_FILE).to_string_lossy().to_string();
+    pub static ref PATH: String = CONFIG_FOLDER.join(*CONFIG_FILE).to_string_lossy().to_string();
 }
-static mut INDEX : Option<Index> = None;
 
-static mut CONFIG : Option<Config> = None;
+static mut INDEX: Option<Index> = None;
+
+static mut CONFIG: Option<Config> = None;
 
 #[derive(Deserialize, Debug)]
-pub struct Config {
-    pub keybinds: Keybinds
-}
+pub struct Config { pub keybinds: Keybinds }
 
 impl Config {
-    
-    pub fn path() -> String {
-        PATH.to_string()
-    }
+    pub fn path() -> String { PATH.to_string() }
 
     ///
     /// Returns the config file's JSON index.
     /// 
     pub fn index<'a>() -> &'a Index {
-        unsafe {
-            INDEX.as_ref().unwrap()
-        }
+        unsafe { INDEX.as_ref().unwrap() }
     }
 
     ///
     /// Returns the Global Configuration Object.
     /// 
     pub fn config<'a>() -> &'a Self {
-        unsafe {
-            CONFIG.as_ref().unwrap()
-        }
+        unsafe { CONFIG.as_ref().unwrap() }
     }
-
 
     ///
     /// Loads the config.
@@ -63,7 +69,8 @@ impl Config {
     /// THIS FUNCTION SHOULD BE NEAR THE TOP OF `main.rs`
     /// 
     pub fn load() -> Result<(), Box<dyn Error>> {
-        let path = PATH.to_string();
+        let path: String = PATH.to_string();
+
         fs::create_dir_all(*CONFIG_FOLDER)
             .expect("Error while creating the AvdanOS config directory!");
     
@@ -77,33 +84,28 @@ impl Config {
 
         let stripped: StripComments<_> = StripComments::new(reader);
 
-        let src_map = {
-            use json_tree::{Tokenizer, Value, Indexer, Source,};
+        let src_map: HashMap<JSONPath, Location> = {
 
-
-            let mut src = Source::new(
+            let mut src: Source = Source::new (
                 // TODO: @Sammy99jsp Prettier Error
                 fs::read_to_string(&path).expect("Missing AvdanOS config!")
             );
 
-            let tokens = match Tokenizer::tokenize(&mut src) {
+            let tokens: Vec<Token> = match Tokenizer::tokenize(&mut src) {
                 Ok(tkns) => tkns,
                 Err(err) => {
-                    let a = UnexpectedToken::from_parser(
-                        err
-                    );
+                    let a: UnexpectedToken = UnexpectedToken::from_parser(err);
 
                     println!("{}", a);
 
                     panic!()
                 }
             };
-            let root = match Value::parse(&mut tokens.iter().peekable()) {
+            let root: Value = match Value::parse(&mut tokens.iter().peekable()) {
                 Ok(r) => r,
+
                 Err(err) => {
-                    let a = UnexpectedToken::from_parser(
-                        err
-                    );
+                    let a: UnexpectedToken = UnexpectedToken::from_parser(err);
 
                     println!("{}", a);
 
@@ -111,22 +113,18 @@ impl Config {
                 }
             };
 
-            let mut index = HashMap::new();
+            let mut index: HashMap<JSONPath, Location> = HashMap::new();
+
             Indexer::index(&root, &mut index, None);
 
             index
         };
 
-        unsafe {
-            INDEX = Some(src_map);
-        }
+        unsafe { INDEX = Some(src_map) }
 
-        let o = serde_json::from_reader(stripped)?;
-        
-        
-        unsafe {
-            CONFIG = Some(o);
-        };
+        let o: Config = serde_json::from_reader(stripped)?;
+    
+        unsafe { CONFIG = Some(o) }
 
         Ok(())
     }
