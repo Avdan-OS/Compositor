d409367c7386b59f30eb5cb43517bc22052f47e9
Akane <96370213+akane6704@users.noreply.github.com>
2022-09-20 09:30:49 +0200

    Guidelines

--- src/macros/src/avvalue.rs
+++ src/macros/src/avvalue.rs
@@ -1,89 +1,108 @@
 use proc_macro2::TokenStream;
-use syn::{Token, parse::ParseStream, Error};
+
 use quote::quote;
 
+use syn::{
+    Error,
+    Ident,
+    LitBool,
+    LitFloat,
+    LitInt,
+    LitStr,
+    parse::{
+        Lookahead1,
+        ParseBuffer,
+        ParseStream,
+    },
+    punctuated::Punctuated,
+    Result,
+    Token,
+    token::Brace,
+};
+
 pub enum AvValue {
-    String(syn::LitStr),
-    Integer(syn::LitInt),
-    Float(syn::LitFloat),
-    Null(syn::Ident),
-    Boolean(syn::LitBool),
-    AvKeys(syn::punctuated::Punctuated<AvKey, Token![+]>),
-    List(syn::punctuated::Punctuated<AvValue, Token![,]>)
+    String(LitStr),
+    Integer(LitInt),
+    Float(LitFloat),
+    Null(Ident),
+    Boolean(LitBool),
+    AvKeys(Punctuated<AvKey, Token![+]>),
+    List(Punctuated<AvValue, Token![,]>)
 }
 
 pub enum AvKey {
-    Key(syn::Ident),
-    Parameter(syn::token::Brace, syn::Ident)
+    Key(Ident),
+    Parameter(Brace, Ident)
 }
 
 impl AvValue {
     pub fn get_type(&self) -> TokenStream {
         use AvValue::*;
         match self {
-            String(_) => quote!{ String },
+            String(_)  => quote!{ String },
             Integer(_) => quote!{ i64 },
-            Float(_) => quote! { f64 },
-            Null(_) => panic!("Null token is not supported for deserialization!"),
+            Float(_)   => quote! { f64 },
+            Null(_)    => panic!("Null token is not supported for deserialization!"),
             Boolean(_) => quote! { bool },
-            AvKeys(_) => quote! { AvKeys },
-            List(_) => panic!("List tokens are not supported for deserialization yet!"),
+            AvKeys(_)  => quote! { AvKeys },
+            List(_)    => panic!("List tokens are not supported for deserialization yet!"),
         }
     }
 
     pub fn value(&self) -> TokenStream {
-        let t = self.get_type();
-        let v = match self {
+        let t: TokenStream = self.get_type();
+        let v: TokenStream = match self {
             AvValue::String(s) => quote! { #s.into() }, 
             AvValue::Integer(s) => quote! { #s.into() }, 
             AvValue::Float(s) => quote! { #s.into() }, 
             AvValue::Null(_) => panic!("Null token is not supported for deserialization!"), 
             AvValue::Boolean(s) => quote! { #s.into() }, 
             AvValue::AvKeys(s) => {
-                let t = s.iter().map(|k| {
+                let t = s.iter().map(|k: &AvKey| {
                     match k {
                         AvKey::Key(k) => {
-                            let k = k.to_string();
-                            quote!{
+                            let k: String = k.to_string();
+
+                            quote! {
                                 AvKey::Key(#k.into())
                             }
                         },
                         AvKey::Parameter(_, p) => {
-                            let p = p.to_string();
-                            quote!{
+                            let p: String = p.to_string();
+
+                            quote! {
                                 AvKey::Parameter(#p.try_into().unwrap())
                             }
                         },
                     }
                 });
 
-                quote!{
-                    AvKeys(
-                        vec![#(#t),*]
-                    )
+                quote! {
+                    AvKeys ( vec![#(#t),*] )
                 }
             },
+
             AvValue::List(_) => panic!("List tokens are not supported for deserialization yet!"),
         };
 
-        quote! {AvValue::#t(#v)}
+        quote! { AvValue::#t(#v) }
     }
 }
 
 impl syn::parse::Parse for AvKey {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let look = input.lookahead1();
+    fn parse(input: ParseStream) -> Result<Self> {
+        let look: Lookahead1 = input.lookahead1();
+
         return match look.peek(syn::Ident) {
             true => {
-                Ok(
-                    AvKey::Key(input.parse()?)
-                )
+                Ok ( AvKey::Key(input.parse()?) )
             },
+            
             false => {
-                let content;
+                let content: ParseBuffer;
 
-                Ok(
-                    AvKey::Parameter(
+                Ok (
+                    AvKey::Parameter (
                         syn::braced!(content in input),
                         content.parse()?
                     )
@@ -119,26 +138,29 @@ impl syn::parse::Parse for AvValue {
         }
 
         if input.peek(syn::token::Paren) {
-            let content;
+            let content: ParseBuffer;
 
             let _ = syn::parenthesized!(content in input);
 
-            return Ok(Self::AvKeys(content.parse_terminated(AvKey::parse)?))
+            return Ok (
+                Self::AvKeys(content.parse_terminated(AvKey::parse)?)
+            )
         }
 
         if input.peek(syn::token::Bracket) {
-            let content;
+            let content: ParseBuffer;
 
             let _ = syn::bracketed!(content in input);
 
-            return Ok(Self::List(content.parse_terminated(AvValue::parse)?));
+            return Ok (
+                Self::List(content.parse_terminated(AvValue::parse)?)
+            );
         }
 
         // let mut e = Diagnostic::new(Level::Error, "Expected a bool, float/int, string, null, or AvKey collection.");
         // e.set_spans(input.span().unwrap());
         // e.emit();
-
-        Err(
+        Err (
             Error::new(input.span(), "Expected a bool, float/int, string, null, or AvKey collection.")
         )
     }
