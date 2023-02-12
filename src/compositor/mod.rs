7c548c93f0cc4c9f6215cc5e336b8e25c9975bfd
Sammy99jsp <sammy99jsp@gmail.com>
2023-02-12 00:35:01 +0000

    Yoinked `winit` backend from anvil -- it just works!

--- src/compositor/mod.rs
+++ src/compositor/mod.rs
@@ -1,6 +1,6 @@
 use slog::{o, Drain, Logger};
 
-use self::backend::run_udev;
+use self::backend::{run_udev, run_winit};
 
 mod backend;
 mod components;
@@ -15,6 +15,16 @@ mod state;
 pub fn start() -> Result<(), Box<dyn std::error::Error>> {
     let plain = slog_term::PlainSyncDecorator::new(std::io::stdout());
     let log = Logger::root(slog_term::FullFormat::new(plain).build().fuse(), o!());
-    run_udev(log);
+
+    // Env variable NAVDA_BACKEND=udev will tell the compositor to launch under udev,
+    // otherwise winit backend.
+    if ::std::env::var("NAVDA_BACKEND")
+        .map(|s| s == "udev").unwrap_or_default()
+        {
+        run_udev(log);
+    } else {
+        run_winit(log);
+    }
+
     Ok(())
 }
