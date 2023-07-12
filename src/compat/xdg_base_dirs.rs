def57425737c7ba860cb5eb254c20ae9de9dca55
Akane <128232444+mrakane7@users.noreply.github.com>
2023-07-12 12:54:01 -0400

    Update xdg_base_dirs.rs

--- src/compat/xdg_base_dirs.rs
+++ src/compat/xdg_base_dirs.rs
@@ -1,19 +1,18 @@
 //!
-//! 
+//!
 //! ## XDG Base Directory (0.8)
 //! The module handles all to do with the XDG Base Diretory Standard.
-//! 
+//!
 //! View the original spec at [specification.freedesktop.org](https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html).
-//! 
+//!
 
 use std::{env, path::PathBuf};
 
 pub trait BaseDir {
-
     ///
-    /// Return the label of the corresponding environment 
+    /// Return the label of the corresponding environment
     /// variable for this base directory.
-    /// 
+    ///
     fn env_variable(&self) -> &'static str;
 
     ///
@@ -21,15 +20,15 @@ pub trait BaseDir {
     /// Called when:
     /// * The folder at the explicitly path does not exist; or
     /// * The corresponding environment vaiable is not set.  
-    /// 
-    /// 
+    ///
+    ///
     fn default(&self) -> PathBuf;
 
     ///
     /// Returns the path of the base directory.
     /// Handles any error cases.
-    /// 
-    /// 
+    ///
+    ///
     fn path(&self) -> PathBuf {
         env::var(self.env_variable())
             .ok()
@@ -43,18 +42,18 @@ pub trait BaseDir {
 
 ///
 /// Wrapper enum over the XDG base directories.
-/// 
+///
 pub enum XdgBaseDir {
     ///
     /// From `$XDG_DATA_HOME`:
     /// > \[The] base directory relative to which user-specific data files should be stored.
-    /// 
+    ///
     Data,
 
     ///
     /// From `$XDG_CONFIG_HOME`:
     /// > \[The] base directory relative to which user-specific configuration files should be stored.
-    /// 
+    ///
     Config,
 
     ///
@@ -64,25 +63,25 @@ pub enum XdgBaseDir {
     /// > Contains state data that should persist between (application) restarts, but that is not important or portable enough to the user that it should be stored in $XDG_DATA_HOME. It may contain:
     /// > * actions history (logs, history, recently used files, …)
     /// > * current state of the application that can be reused on a restart (view, layout, open files, undo history, …)
-    /// 
-    State
+    ///
+    State,
 }
 
 impl BaseDir for XdgBaseDir {
     fn env_variable(&self) -> &'static str {
         match self {
-            Self::Data      => "XDG_DATA_HOME",
-            Self::Config    => "XDG_CONFIG_HOME",
-            Self::State     => "XDG_STATE_HOME",
+            Self::Data => "XDG_DATA_HOME",
+            Self::Config => "XDG_CONFIG_HOME",
+            Self::State => "XDG_STATE_HOME",
         }
     }
 
     fn default(&self) -> PathBuf {
-        let mut path : PathBuf = env::var("HOME")
-            .expect("NO $HOME SET! YOU MONSTER!")
-            .into();
+        let mut path: PathBuf = env::var("HOME").expect("NO $HOME SET! YOU MONSTER!").into();
 
-        if !path.exists() { panic!("$HOME INEXISTANT -- PANIC!"); }
+        if !path.exists() {
+            panic!("$HOME INEXISTANT -- PANIC!");
+        }
 
         match self {
             XdgBaseDir::Data => path.push(".local/share"),
