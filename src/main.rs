30c81513400baa8f0c88c6e45f0a278285fad44f
Sammy99jsp <sammy99jsp@sammy99jsp.dev>
2024-04-19 21:45:33 +0100

    Add `color_eyre` for panic handling.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use crate::utils::LOG;
 mod utils;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
     info!(LOG, "Logging ready!");
+
+    Ok(())
 }
